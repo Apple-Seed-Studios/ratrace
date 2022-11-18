ec50b8779c0521d332333927636dd408193da128
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-11-18 22:48:32 +0000

    moved License to root | added to gitignore | added Auth0 provider to index.js

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -4,13 +4,14 @@ import store from './store';
 import TaskForm from './components/TaskForm'
 import TaskDisplay from './components/TaskDisplay';
 
-function App() {
+function App()
+{
   return (<>
-  <Provider store={store}>
-  <TaskForm/>
-  <TaskDisplay/>
-  <LandingPage/>
-  </Provider>
+    <Provider store={ store }>
+      <TaskForm />
+      <TaskDisplay />
+      <LandingPage />
+    </Provider>
   </>);
 }
 
--- web-client/src/index.js
+++ web-client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// import auth0 dependencies
+import { Auth0Provider } from "@auth0/auth0-react";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain={ process.env.REACT_APP_AUTH_DOMAIN }
+    clientId={ process.env.REACT_APP_AUTH_CLIENT_ID }
+    redirectUri={ process.env.REACT_APP_AUTH_REDIRECT_URI }
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
