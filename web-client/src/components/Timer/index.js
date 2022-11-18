fa63cf6111002f7da16a7a1d5524c4213dd40f6a
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-11-18 21:57:40 +0000

    Added timer component and corresponding store

--- web-client/src/components/Timer/index.js
+++ web-client/src/components/Timer/index.js
@@ -1,14 +1,52 @@
-import { useSelector } from 'react-redux';
-import { Fab } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { Fab, TextField } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit'
+import { useState, useEffect } from 'react';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { decrementTime, setTime } from '../../store/timer';
+import PauseIcon from '@mui/icons-material/Pause';
+
 
 const Timer = function () {
 
     let time = useSelector(state => state.time)
 
+    let [toggleEdit, setToggleEdit] = useState(false);
+    let [intervalId, setIntervalId] = useState();
+    let dispatch = useDispatch();
+
+    let handleToggle = () => {
+        setToggleEdit(!toggleEdit);
+    }
+    let startTimer = () => {
+        setIntervalId(setInterval(countDown, 1000))
+    }
+    let stopTimer = () => {
+        clearInterval(intervalId)
+    } 
+    let countDown = () => {
+        dispatch(decrementTime());
+    }
+    useEffect(() => {
+        if(time <= 0){
+            clearInterval(intervalId)
+        }
+    }, [time, intervalId])
+
+
+
+    let convertTime = (milliseconds) => {
+        let totalSeconds = milliseconds /1000;
+        let totalMinutes = Math.floor(totalSeconds / 60)
+        let leftOverSeconds = Math.floor(totalSeconds % 60)
+        return totalMinutes + ':' + leftOverSeconds;
+    }
+
     return (<>
-    <h1>{time.time}</h1>
-    <Fab><EditIcon/></Fab>
+    {toggleEdit ? <TextField  defaultValue={convertTime(time)}/>  : <h1>{convertTime(time)}</h1>}
+    <Fab onClick={handleToggle} size='small'><EditIcon/></Fab>
+    <Fab size='small' onClick={startTimer}><PlayArrowIcon/></Fab>
+    <Fab size='small' onClick={stopTimer}><PauseIcon/></Fab>
     </>)
 }
 
--- web-client/src/store/timer.js
+++ web-client/src/store/timer.js
@@ -1,25 +1,23 @@
-let time = {
-    time: 10000,
-}
+let time = 3000
 
 let setTime = (payload) => {
     return {
-        action: "SET_TIME",
+        type: "SET_TIME",
         payload: payload,
     }
 }
 
 let decrementTime = (payload) => {
     return {
-        action: "DECREMENT_TIME",
+        type: "DECREMENT_TIME",
         payload: payload,
     }
 }
 
 let timeReducer = (state=time, action) => {
     switch(action.type){
-        case "SET_TIME": return state.time = action.payload;
-        case "DECREMENT_TIME": return state.time - 1000
+        case "SET_TIME": return time = action.payload;
+        case "DECREMENT_TIME": return state - 1000
         default: return state;
     }
 }
@@ -27,4 +25,5 @@ let timeReducer = (state=time, action) => {
 module.exports = {
     setTime,
     timeReducer,
+    decrementTime,
 }
