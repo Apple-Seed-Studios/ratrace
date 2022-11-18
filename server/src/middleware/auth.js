83264bbab8306613b2a7786f8fe4b5f07c742e24
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-11-18 19:09:35 +0000

    added starter files | added .env.sample | updated comments in auth.js

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,17 +1,23 @@
 'use strict';
 
-// jwt - JSON web token (pronounced JOT)
+/* REQUIRE */
+// jwt - JSON Web Token package
+// pronounced: 'jot'
 const jwt = require('jsonwebtoken');
 
-// jwks - JSON web key set (pronounced Ja-wicks)
+// jwks - JSON Web Key Set package
+// (pronounced Ja-wicks)
 const jwksClient = require('jwks-rsa');
 
-// the jwks uri come Auth0 account page -> advanced settings -> Endpoints -> 0auth -> JSON Web Key Set
+// don't forget to 'npm i jsonwebtoken jwks-rsa
+
+// the jwks uri comes from Auth0 account page -> advanced settings -> Endpoints -> 0auth -> JSON Web Key Set
 const client = jwksClient({
-  jwksUri: process.env.JWKS_URI
+  jwksUri: process.env.JWKS_URI,
 });
 
-// from the jsonwebtoken docs
+// this function will process the key and do the magic for us
+// function is from the jsonwebtoken docs
 // https://www.npmjs.com/package/jsonwebtoken
 // (search for auth0)
 function getKey(header, callback){
@@ -21,16 +27,22 @@ function getKey(header, callback){
   });
 }
 
-// function to verify the user on our route
+// function to verify's token
 // this is just how we do it
+// errorFirstOrUserCallback is a callback function to just deny a request if the requester isn't who they say they are without even trying to run the function
 function verifyUser (req, errorFirstOrUserCallback) {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    // the docs
+    // get the auth0/jwt token from the client's request
+    // the `.verify()` method is from the jwt package we imported/required
+    // `getKey` is the function above this one, from the jwt docs
+    // go to https://www.npmjs.com/package/jsonwebtoken
+    // use `ctrl+f` and search for: `Verify using getKey callback` to get an example
     jwt.verify(token, getKey, {}, errorFirstOrUserCallback);
   } catch (error) {
-    errorFirstOrUserCallback('not authorized');
+    errorFirstOrUserCallback("You're not the rat we were expecting");
   }
 }
 
+// export our 'verifyUser' function to use when a client attempts to connect to a socket
 module.exports = verifyUser;
