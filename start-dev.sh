6bd1c79acc0808f4697b2270bf00f353eb73bcc9
Robert Mccreary <fooyinfighter@gmail.com>
2022-11-19 15:49:32 -0800

    dev start script

---
+++ start-dev.sh
@@ -0,0 +1,9 @@
+cd web-client; npm ci; npm run start &
+react_app_ps=$!
+cd ../server; npm ci; npm run dev
+node_app_ps=$!
+
+echo "don't forget to kill the processes when finished!"
+echo react_app_ps
+echo node_app_ps
+
