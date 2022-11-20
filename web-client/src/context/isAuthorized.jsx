05247b8d1d4e0448c7fa4bc5faf9c542611229e6
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-11-20 08:55:36 +0000

    added functions to get bearer token from Auth0 and template functions to make API requests with the token

--- web-client/src/context/isAuthorized.jsx
+++ web-client/src/context/isAuthorized.jsx
@@ -1,14 +1,44 @@
-import { useSelector } from 'react-redux';
 import { When } from 'react-if';
 import { useAuth0 } from "@auth0/auth0-react";
 
 function IsAuthorized(props)
 {
-    const { isAuthenticated } = useAuth0();
-    let loggedIn = useSelector(state => state.loggedIn);
+    const { isAuthenticated, user } = useAuth0();
+
+    const getAuthClaims = async () =>
+    {
+        if (!isAuthenticated) return ('not authenticated');
+
+        return useAuth0.getIdTokenClaims().then(claims => claims.__raw).catch(err =>
+        {
+            console.log("Something went wrong getting the auth claims", err);
+        })
+    }
+
+
+    // this is a template for creating a request with the token
+    // since different CRUD requires a different config, I think this is a good candidate for a reducer/redux!
+    const requestTemplate = (jwt) =>
+    {
+        const config = {
+            method: 'get',
+            baseURL: process.env.REACT_APP_SERVER,
+            url: '/letters',
+            headers: {
+                "email": `${ user.email }`,
+
+                // pass token into the headers
+                "Authorization": `Bearer ${ jwt }`
+            }
+        }
+        return config;
+    };
+
+    // after building the config, we can call axios like this:
+    let templateResponse = async (config) => await axios(config);
 
     return (
-        <When condition={ loggedIn }>
+        <When condition={ isAuthenticated }>
             { props.children }
         </When>)
 }
