21865512ddfebfcea4e89e74a21c14c9d9285495
Robert Mccreary <fooyinfighter@gmail.com>
2022-11-21 10:04:56 -0800

    TaskForm tests

--- web-client/src/components/TaskForm/TaskForm.jsx
+++ web-client/src/components/TaskForm/TaskForm.jsx
@@ -1,32 +1,30 @@
 import Button from '@mui/material/Button'
 import { Dialog, DialogTitle, DialogContent } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useState } from 'react';
 import TaskFromContent from './TaskFormContent';
 import { addTask } from '../../store/tasks';
 
 
-const TaskForm = function ()
-{
+const TaskForm = function () {
 
     let dispatch = useDispatch();
-    let tasks = useSelector(state => state.tasks)
+    // let tasks = useSelector(state => state.tasks)
     let [ showForm, setShowForm ] = useState(false);
 
-    let handleSubmit = (e) =>
-    {
+    let handleSubmit = (e) => {
         e.preventDefault();
         dispatch(addTask({
-            task_name: e.target.task_name.value,
-            task_description: e.target.task_description.value,
+            task_name: e.target.elements.task_name.value,
+            task_description: e.target.elements.task_description.value,
+//            task_name: e.target.task_name.value,
+//            task_description: e.target.task_description.value,
         }))
-        console.log(tasks)
-    }
-    const toggleForm = () =>
-    {
-        setShowForm(!showForm);
     }
 
+    const toggleForm = () => {
+        setShowForm(!showForm);
+    }
 
     return (<>
     <Dialog open={showForm} onClose={toggleForm}>
---
+++ web-client/src/components/TaskForm/TaskForm.test.jsx
@@ -0,0 +1,69 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+import '@testing-library/jest-dom';
+import TaskForm from './TaskForm';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware  } from 'redux'
+import thunk from 'redux-thunk';
+
+
+let store;
+
+const mockReducer = jest.fn((state, action) => state);
+
+describe('Test TaskForm Component', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    store = createStore(mockReducer, applyMiddleware(thunk));
+  });
+
+  test('should render TaskForm', () => {
+    render(
+      <Provider store={store}>
+        <TaskForm />
+      </Provider>
+    )
+    expect(screen.getByText(/\+ Task/)).toBeInTheDocument();
+  });
+
+  test('should open on button click', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Provider store={store}>
+        <TaskForm />
+      </Provider>
+    )
+
+    const openButton = screen.getByText(/\+ Task/);
+    await user.click(openButton);
+    expect(await screen.findByText(/Add Task/i)).toBeInTheDocument();
+  });
+
+  test('should call dispatch on submit', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Provider store={store}>
+        <TaskForm />
+      </Provider>
+    )
+
+    const openButton = screen.getByText(/\+ Task/);
+    await user.click(openButton);
+    expect(await screen.findByText(/Add Task/i)).toBeInTheDocument();
+    const taskNameInput = screen.getByLabelText(/Task Name/);
+    const taskDescInput = screen.getByLabelText(/Description/);
+    const taskSubmitButton = screen.getByText(/Submit/);
+    await user.click(taskNameInput);
+    await user.keyboard("Buy bread");
+    await user.click(taskDescInput);
+    await user.keyboard("Pumpkin seed");
+    await user.click(taskSubmitButton);
+    expect(mockReducer).toHaveBeenCalled();
+  });
+
+});
