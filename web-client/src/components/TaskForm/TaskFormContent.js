11f2d73c7c58f23d69e1359fd4e2a629615ebb06
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-12-08 01:37:44 +0000

    Styled the task form component, and added functionality to support multiple tags

--- web-client/src/components/TaskForm/TaskFormContent.js
+++ web-client/src/components/TaskForm/TaskFormContent.js
@@ -1,23 +1,16 @@
-import {TextField, Button, Fab, Chip} from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import { useState } from 'react'
+import {TextField, Button, Fab, Chip, Grid} from '@mui/material';
+import { useState } from 'react';
+import { addTask } from '../../store/tasks';
+import { useDispatch } from 'react-redux'
 
-const style = {
-  textField: {
-    display: 'grid',
-    padding: '5px',
-    width: '500px'
-  },
-  InputProps: {
-    disableUnderline: true,
-  },
-  button: { margin: '10px' }
-};
-
-function TaskFromContent({ handleSubmit, toggleForm }) {
+function TaskFromContent({toggleForm }) {
 
   let [tagFormOpen, setTagFormOpen] = useState(false)
   let [tempTags, setTempTags] = useState([]);
+  let [taskName, setTaskName] = useState('');
+  let [taskDescription, setTaskDescription] = useState('');
+
+  let dispatch = useDispatch();
 
   let handleTags = (e) => {
     e.preventDefault();
@@ -29,23 +22,47 @@ function TaskFromContent({ handleSubmit, toggleForm }) {
     setTempTags(newTags)
   }
 
+  let handleSubmit = () => {
+    dispatch(addTask({
+        task_name: taskName,
+        task_description: taskDescription,
+        tracked_time: 0,
+        tag: tempTags,
+    }))
+    setTempTags([])
+    setTaskName('')
+    setTaskDescription('')
+}
+
   return (<>
-    <form onSubmit={handleSubmit}>
-      <TextField style={ style.textField } InputProps={style.InputProps} variant='standard' label='Task Name' id='task_name' required></TextField>
-      <TextField style={ style.textField } InputProps={style.InputProps} variant='standard' label='Description' id='task_description' required></TextField>
-      {tempTags.map((tag, idx) => {
-        console.log(tag)
-        return (<Chip key={idx} label={tag} onDelete={() => handleTagDelete(tag)}></Chip>)
-      })}
-      <Button style={ style.button } type='submit' onClick={toggleForm} variant='outlined'>Save</Button>
-    </form>
-    {tagFormOpen 
-      ?<form onSubmit={handleTags}>
-        <TextField  InputProps={style.InputProps} variant='outlined' label='Tag' id='tags' required></TextField>
-        <Button type='submit'>Add Tag</Button>
-        <Button onClick={() => setTagFormOpen(false)}>X</Button>
-      </form>
-      :<Fab style={{transform: 'scale(0.5)'}} variant='extended' onClick={() => setTagFormOpen(true)}><AddIcon sx={{fontSize: 20}}/> Tag</Fab>}
+  <Grid container spacing={2}>
+        <Grid item xs={10}>
+      <TextField fullWidth onChange={(e) => setTaskName(e.target.value)} variant='outlined' label='Task Name' id='task_name' required></TextField>
+      </Grid>
+      <Grid item xs={2}>
+      <Button sx={{ marginTop:'15%', width: '50%'}}size='small' onClick={() => {toggleForm(); handleSubmit(); }} variant='outlined'>Save</Button>
+      </Grid>
+      <Grid item xs={10}>
+      <TextField fullWidth multiline rows={4} onChange={(e)=> setTaskDescription(e.target.value)}variant='outlined' label='Description' id='task_description' required></TextField>
+      </Grid>
+        {tagFormOpen 
+        ?<form onSubmit={handleTags}>
+          <TextField variant='outlined' label='Tag' id='tags' required></TextField>
+          <Button type='submit'>Add Tag</Button>
+          <Button onClick={() => setTagFormOpen(false)}>X</Button>
+        </form>
+        :<Grid item xs={2}>
+        <Chip color='primary' label='+ Tag' onClick={() => setTagFormOpen(true)}></Chip>
+        </Grid>
+        }
+      <Grid item xs={10}>
+        {tempTags.map((tag, idx) => {
+          console.log(tag)
+          return (<Chip key={idx} label={tag} onDelete={() => handleTagDelete(tag)}></Chip>)
+        })}
+
+    </Grid>
+    </Grid>
     </>
    );
 }
