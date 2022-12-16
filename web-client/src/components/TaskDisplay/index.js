85998c36f6316eb330635f4ebb5d1d853b389930
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-12-15 17:23:38 -0800

    Refactored uncompleted task display to use the list component from material ui

--- web-client/src/components/TaskDisplay/index.js
+++ web-client/src/components/TaskDisplay/index.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { convertTimeReadable } from "../../hooks/convertTime";
 import { getTasks, deleteTask, updateTask } from "../../store/tasks";
 import { setActiveTask } from "../../store/activeTask";
@@ -124,17 +124,17 @@ const TaskDisplay = function () {
                     </Typography>
                     {task.tag
                       ? console.log(
-                          task.tag.map((t) => {
-                            return (
-                              <Typography variant="subtitle2">{t}</Typography>
-                            );
-                          })
-                        )
+                        task.tag.map((t) => {
+                          return (
+                            <Typography variant="subtitle2">{t}</Typography>
+                          );
+                        })
+                      )
                       : []}
                     <Typography variant="subtitle1">
                       {activeTask && task._id === activeTask._id
                         ? convertTimeReadable(activeTask.tracked_time)
-                            .minutesSeconds
+                          .minutesSeconds
                         : convertTimeReadable(task.tracked_time).minutesSeconds}
                     </Typography>
                   </CardActionArea>
@@ -154,72 +154,63 @@ const TaskDisplay = function () {
         </Then>
         <Else>
           <List>
-          {tasks
-            .filter((task) => !task.complete)
-            .map((task) => {
-              return (
-                // <Card
-                //   variant="outlined"
-                //   sx={{ backgroundColor: "#424242", marginBottom: 2 }}
-                //   id={task._id}
-                //   key={task._id}
-                // >
-                <ListItem>
-                  {/* <CardActionArea> */}
-                    {/* <CardContent
-                      onClick={() => {
-                        setCurrentEdit(task);
-                        setModalOn(true);
-                      }}
-                    > */}
-                      {/* <CardActionArea> */}
-                        {/* <Typography variant="h5">{task.task_name}</Typography> */}
-                        <ListItemIcon>                  <Fab
-                    size="small"
-                    onClick={() => dispatch(setActiveTask(task))}
+            {tasks
+              .filter((task) => !task.complete)
+              .map((task) => {
+                return (
+                  <ListItem
+                    divider
+                    disablePadding
+                    secondaryAction={
+                      <IconButton onClick={() => dispatch(deleteTask(task))}>
+                        <ClearIcon />
+                      </IconButton>}
                   >
-                    <PlayArrowIcon />
-                  </Fab></ListItemIcon>
-                        <ListItemText primary={task.task_name} secondary={task.task_description}></ListItemText>
-                        {/* <Typography variant="body1">
-                          {task.task_description}
-                        </Typography> */}
-                        {task.tag
-                          ? task.tag.map((t) => {
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <Fab
+                          size="small"
+                          onClick={() => dispatch(setActiveTask(task))}
+                        >
+                          <PlayArrowIcon />
+                        </Fab>
+                        <IconButton onClick={(event) => completeTask(event, task)}>
+                          <CheckIcon />
+                        </IconButton>
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={
+                          <React.Fragment>
+                            <Typography variant='h6' sx={{ display: 'inline', }}>
+                              {task.task_name + ' '}
+                            </Typography>
+                            {task.tag.map(tag => {
                               return (
-                                <Typography variant="subtitle2">
-                                  #{t}
+                                <Typography variant="caption" sx={{ display: 'inline', fontStyle: 'italic' }}>
+                                  #{tag} {' '}
                                 </Typography>
-                              );
-                            })
-                          : []}
-                        <Typography variant="subtitle1">
-                          {activeTask && task._id === activeTask._id
-                            ? convertTimeReadable(activeTask.tracked_time)
-                                .minutesSeconds
-                            : convertTimeReadable(task.tracked_time)
-                                .minutesSeconds}
-                        </Typography>
-                      {/* </CardActionArea> */}
-                    {/* </CardContent> */}
-                  {/* </CardActionArea> */}
-                  {/* <Fab
-                    size="small"
-                    onClick={() => dispatch(setActiveTask(task))}
-                  >
-                    <PlayArrowIcon />
-                  </Fab> */}
-                  <IconButton onClick={(event) => completeTask(event, task)}>
-                    <CheckIcon />
-                  </IconButton>
-                  <IconButton onClick={() => dispatch(deleteTask(task))}>
-                    <ClearIcon />
-                  </IconButton>
-                {/* </Card> */}
-                </ListItem>
-              );
-            })}
-            </List>
+                              )
+                            })}
+                          </React.Fragment>
+                        }
+                        secondary={task.task_description}
+                        onClick={() => {
+                          setCurrentEdit(task);
+                          setModalOn(true);
+                        }}
+                      ></ListItemText>
+                      <Typography sx={{ marginRight: 2 }} variant="subtitle1">
+                        {activeTask && task._id === activeTask._id
+                          ? convertTimeReadable(activeTask.tracked_time)
+                            .minutesSeconds
+                          : convertTimeReadable(task.tracked_time)
+                            .minutesSeconds}
+                      </Typography>
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+          </List>
         </Else>
       </If>
       {modalOn ? editForm() : []}
