a6566b9241e1360967ee79344aa5003b8d653106
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-12-22 23:21:06 +0000

    Added unit tests for our Copyright component.

--- web-client/src/components/Footer/Copyright/Copyright.test.jsx
+++ web-client/src/components/Footer/Copyright/Copyright.test.jsx
@@ -4,8 +4,19 @@ import Copyright from ".";
 
 describe("Test Copyright Component", () => {
   test("Copyright Component renders", () => {
-    render(<Copyright />);
+    render(<Copyright></Copyright>)
     const expectedContent = "Apple Seed Studios 🐀";
     expect(screen.getByText(expectedContent)).toBeInTheDocument();
+
   });
+  test("Copyright should link off of the page to our github.", () =>{
+    render(<Copyright></Copyright>)
+    const expectedContent = "Apple Seed Studios 🐀";
+    expect(screen.getByText(expectedContent).href).toEqual("https://github.com/Apple-Seed-Studios")
+  });
+  test("Copyright should display the current date.", () => {
+    render(<Copyright></Copyright>);
+    let expectedDate = new RegExp(new Date().getFullYear());
+    expect(screen.getByText(expectedDate)).toBeInTheDocument();
+  })
 });
