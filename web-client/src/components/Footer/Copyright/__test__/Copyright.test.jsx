b863dbbd90fb6b51085b766023071c05a4721be8
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-12-22 23:40:05 +0000

    Added a test for our Footer, and moved it into a test folder.

--- web-client/src/components/Footer/Copyright/__test__/Copyright.test.jsx
+++ web-client/src/components/Footer/Copyright/__test__/Copyright.test.jsx
@@ -1,6 +1,6 @@
 import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Copyright from ".";
+import Copyright from "..";
 
 describe("Test Copyright Component", () => {
   test("Copyright Component renders", () => {
