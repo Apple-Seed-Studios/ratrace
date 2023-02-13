db3ff805e9eeb82344c17e874a45f0b517fe4f3c
Robert Mccreary <fooyinfighter@gmail.com>
2023-02-13 12:22:38 -0500

    add puppeteer tests for splash and about pages

--- e2e-tests/tests/splash-page.test.js
+++ e2e-tests/tests/splash-page.test.js
@@ -1,17 +1,26 @@
 const timeout = 5000;
 
 describe(
-  '/ (Home Page)',
+  '/ (Splash Page)',
   () => {
     let page;
     beforeAll(async () => {
       page = await globalThis.__BROWSER_GLOBAL__.newPage();
-      await page.goto('https://google.com');
+      await page.goto('http://localhost:3000');
     }, timeout);
 
     it('should load without error', async () => {
       const text = await page.evaluate(() => document.body.textContent);
-      expect(text).toContain('google');
+      expect(text).toContain('Rat-race');
+    });
+    it('should have an about button', async () => {
+      const aboutButton = await page.$('a[href="/about"]');
+      expect(aboutButton).toBeTruthy();
+    });
+    it('should navigate to the about page', async () => {
+      await page.click('a[href="/about"]');
+      const text = await page.evaluate(() => document.body.textContent);
+      expect(text).toContain('We are Apple Seed Studios');
     });
   },
   timeout,
